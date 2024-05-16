cbc85b721041544b2ff391efb1ebbc6b52058839
Pifocode <andrey.shinkarenko.04@mail.ru>
2024-05-16 17:55:24 +0300

    fix

--- heat.cu
+++ heat.cu
@@ -52,7 +52,7 @@ __global__ void heat_kernel(int nx, int ny, int nz, float* d_Un, float* d_Unp1, 
                 float uijkp1 = d_Un[getIndex(i, j, k+1, ny, nz)];
                 
                 // Explicit scheme
-                d_Unp1[index] = uij + aTimesDt * ( (uim1jk - 2.0*uij + uip1jk)/dx2 + (uijm1k - 2.0*uij + uijp1k)/dy2 + (uijkm1 - 2.0*uij + uijkp1)/dz2);
+                d_Unp1[index] = uij + aTimesDt * ( (uim1jk - 2.0*uij + uip1jk)/dx2 + (uijm1k - 2.0*uij + uijp1k)/dy2);
             }
         }
     }
@@ -77,7 +77,7 @@ int main()
     const float dy2 = dy*dy;
     const float dz2 = dz*dz;
 
-    const float dt = dx2 * dy2 / (2.0 * a * (dx2 + dy2)); // Largest stable time step
+    const float dt = dx2 * dy2 * dz2/ (2.0 * a * (dx2 + dy2 + dz2)); // Largest stable time step
     const int numSteps = 500000;                                       // Number of time steps
     const int outputEvery = 1000;                                    // How frequently to write output image
 
